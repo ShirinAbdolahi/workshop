f3db3b3d7af2a87d6021092c0ff8664583cfe6c5
Shirin_Abdolahi <abdolahi@outlook.com>
2015-05-20 17:15:48 +0430

    test

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 var app = {
     showAlert: function(message, title) {
         if (navigator.notification) {
+            navigator.notification.alert('message', null, title, 'OK');
             navigator.notification.alert(message, null, title, 'OK');
         } else {
             alert(title ? (title + ": " + message) : message);
